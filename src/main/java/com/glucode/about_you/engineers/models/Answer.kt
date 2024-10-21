c1814ac7da8402d3d02dd5954e00c1cfff0ad9c1
PrinceFana <109230330+PrinceFana@users.noreply.github.com>
2024-10-21 14:38:20 +0200

    Revert "Add files via upload"
    This reverts commit 201de96bc533162abc13af60bf729ab28c54aaed.

--- src/main/java/com/glucode/about_you/engineers/models/Answer.kt
+++
@@ -1,6 +0,0 @@
-package com.glucode.about_you.engineers.models
-
-data class Answer(
-    val text: String?,
-    val index : Int?
-)
--- src/main/java/com/glucode/about_you/engineers/models/QuickStats.kt
+++
@@ -1,7 +0,0 @@
-package com.glucode.about_you.engineers.models
-
-data class QuickStats(
-    val years: Int,
-    val coffees: Int,
-    val bugs: Int
-)
